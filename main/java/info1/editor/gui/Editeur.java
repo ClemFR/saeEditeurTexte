a968efd48a04e8145630e6e2289071f3281c303e
gabriel monge <zoolk.gabi@gmail.com>
2022-05-18 10:18:39 +0200

    init 2.0

--- main/java/info1/editor/gui/Editeur.java
+++ main/java/info1/editor/gui/Editeur.java
@@ -1,10 +1,12 @@
+package info1.editor.gui;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Editeur extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+
     }
 
     @Override
