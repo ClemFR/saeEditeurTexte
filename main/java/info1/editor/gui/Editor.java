b8d9305d8ca6767195bd467145f238e0166602a6
CacheCaille <zoolk.gabi@gmail.com>
2022-05-25 17:43:22 +0200

    Création fenetre commande et QOL

--- main/java/info1/editor/gui/Editor.java
+++ main/java/info1/editor/gui/Editor.java
@@ -1,10 +1,16 @@
 package info1.editor.gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-
+import info1.editor.gui.EditorController;
 import java.io.IOException;
 
 /**
@@ -15,10 +21,14 @@ import java.io.IOException;
 public class Editor extends Application{
 
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Editor.class.getResource("EditeurScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 480);
+
+
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
---
+++ main/java/info1/editor/gui/EditorController.java
@@ -0,0 +1,92 @@
+/*
+ * EditorController.java             18/05/2022
+ * IUT de Rodez, Groupe 3, pas de copyright
+ */
+package info1.editor.gui;
+
+// import static info1.editor.gui.Editor.stageBis;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import info1.editor.gui.Editor;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+/**
+ * TODO commenter la responsabilité de cette classe
+ *  *
+ */
+public class EditorController {
+
+    @FXML
+    private TextArea textShow;
+    @FXML
+    public TextField textCommand;
+
+    protected Stage popup = new Stage();
+
+    @FXML
+    protected void onSendAction() {
+        quit();
+    }
+
+    @FXML
+    protected void onHelpAction() {
+        if (popup.isShowing()) {
+            popup.close();
+        } else {
+            helpStage();
+        }
+    }
+
+    private void quit() {
+        if (textCommand.getText().trim().equals("q")) {
+            Platform.exit();
+        }
+    }
+    private void helpStage() {
+        popup.setTitle("Commands");
+        popup.setHeight(620);
+        popup.setWidth(330);
+        popup.setX(150);
+        VBox racine = new VBox(10);
+        Text titre = new Text("WARNING: Arguments séparés par des virgules\n" +
+                "\n" +
+                "> t un_fichier : lit le contenu du fichier.\n" +
+                "\n" +
+                "> f un_fichier : écrit le contenu du tampon \nqui contient le fichier.\n" +
+                "\n" +
+                "> q : permet de quitter l’éditeur.\n" +
+                "\n" +
+                "> e : efface les lignes mises en argument.\n" +
+                "\n" +
+                "> a : insère le texte après la ligne mise en argument.\n" +
+                "\n" +
+                "> i : insère le texte tapé sous  ligne 1 \nmise en argument.\n" +
+                "\n" +
+                "> m : modifie la ligne mise en argument par le texte \ntapé après la commande exécutée.\n" +
+                "\n" +
+                "> c : copie la ligne ou l’intervalle de lignes mis en \nargument à un emplacement écrit après celle-ci.\n" +
+                "\n" +
+                "> h : possède les mêmes fonctions que la commande \nci-dessus sans faire de distinction entre \nles majuscules et les minuscules.\n" +
+                "\n" +
+                "> r : effectue une recherche de la chaîne de caractères \nentrée en argument dans l’intervalle de lignes qui précède \ncelle-ci. " +
+                "Il est possible de ne pas mettre d’intervalles. \nAlors la recherche s’effectuera dans tout le texte.\n" +
+                "\n" +
+                "> s : substitue une chaine de caractères par une autre qui \nsuit celle-ci en argument dans l’intervalle de lignes entré \nen premier argument. " +
+                "Il est possible de ne pas mettre \nd’intervalles. Alors la substitution s’effectuera \ndans tout le texte.\n" +
+                "\n");
+        racine.getChildren().add(titre);
+        Scene scene = new Scene(racine);
+        popup.setScene(scene);
+        popup.show();
+
+    }
+}
